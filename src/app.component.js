cf31d1c8586934a825c1095a154839dbfe7b945c
Mark Small <mark.small@astrosat.space>
2020-02-24 22:03:30 +0000

    fix(frontend): Rename files to follow our naming structure
    I use `package by feature`, and indicate the type of module in the
    filename e.g. *.component.js.
    
    I also switched to using CSS Modules instead of plain CSS, this helps
    encapsulate the CSS better.

---
+++ src/app.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
