84abf81be6aa61b4796c01b62e8f6dfba99755eb
Mark Small <mark.small@astrosat.space>
2020-02-24 22:09:53 +0000

    feat(frontend): Add Hot Module reloading
    This is purely a development feature, it allows us to reload individual
    chunks of the app, instead of reloading the whole page. This  means we
    don't lose state while developing.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app.component';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  const App = require('./app.component').default;
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app.component', render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
